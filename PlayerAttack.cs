d98f6b33390d5f17925d9caf1c102c540311315b
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-15 09:32:12 -0500

    Animation can trigger on space bar

---
+++ PlayerAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+    public float attackTime;
+    public float startTimeAttack;
+
+    public Transform attackLocation;
+    public float attackRange;
+    public LayerMask enemies;
+    private bool playerAttacking;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            attackTime = startTimeAttack;
+            playerAttacking == true;
+            anim.SetBool("PlayerAttacking", true);
+            Collider2D[] damage = Physics2D.OverlapCircleAll( attackLocation.position, attackRange, enemies );
+        } 
+        if (attackTime > 0) {
+        attackTime -= Time.deltaTime;
+
+        }
+        if (attackTime <= 0){
+            attackTime = 0;
+            anim.SetBool("PlayerAttacking", false);
+        
+            // for (int i = 0; i < damage.Length; i++){
+            //         Destroy( damage[i].gameObject );
+            //     }
+            // }
+        }
+    }
+        private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackLocation.position, attackRange);
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private bool playerMoving;
     private Vector3 lastMove;
+    private bool playerAttacking;
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +25,16 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         playerMoving = false;
+        // playerAttacking = false;
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
 
         movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
         movementDirection.Normalize();
-
+        
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
             playerMoving = true;
             lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
             rb.velocity = movementDirection * movementSpeed * baseSpeed;
@@ -41,17 +42,23 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
             playerMoving = true;
             lastMove = new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
             rb.velocity = movementDirection * movementSpeed * baseSpeed;
         }
-        if (playerMoving == false)
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-        }
+        if (playerMoving == false){
+            rb.velocity = new Vector3(0, 0, 0); 
+        };
+        // if (Input.GetKeyDown("space")) {
+        //     Debug.Log("Space key pressed");
+        //     playerAttacking = true;
+        // };
+
+            
+        // anim.SetBool("PlayerAttacking", playerAttacking);
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.z);
+
     }
 }
